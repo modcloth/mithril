ad1d06c01371c9b9775dcb9d8a7a952aa0eaff96
Dan Buch <d.buch@modcloth.com>
2013-05-04 00:04:12 -0400

    Pulling the HTTP request wrapper into its own file

--- request.go
+++ request.go
@@ -1,7 +1,6 @@
 package mithril
 
 import (
-	"fmt"
 	"io"
 	"net/http"
 	"net/url"
@@ -13,28 +12,4 @@ type Request interface {
 	Query() url.Values
 	Headers() *http.Header
 	String() string
-}
-
-type HTTPRequestWrapper struct {
-	Req *http.Request
-}
-
-func (me *HTTPRequestWrapper) Body() io.Reader {
-	return me.Req.Body
-}
-
-func (me *HTTPRequestWrapper) Headers() *http.Header {
-	return &me.Req.Header
-}
-
-func (me *HTTPRequestWrapper) Path() string {
-	return me.Req.URL.Path
-}
-
-func (me *HTTPRequestWrapper) String() string {
-	return fmt.Sprintf("%s %s %s", me.Req.Method, me.Req.URL.Path, me.Req.Proto)
-}
-
-func (me *HTTPRequestWrapper) Query() url.Values {
-	return me.Req.URL.Query()
 }
